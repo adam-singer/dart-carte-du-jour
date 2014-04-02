9f550e76750b607a9c86c5daf32b27f183992385
Adam Singer <financeCoding@gmail.com>
2014-04-02 13:44:14 -0700

    scripts that now deploy a dart project and launch the service.

--- scripts/quick_launch/launch-instance.sh
+++ scripts/quick_launch/launch-instance.sh
@@ -1,4 +1,4 @@
 set -x
 
 STARTUP_SCRIPT=startup-script.sh
-gcutil --service_version="v1" --project="dart-carte-du-jour" addinstance "test-instance" --zone="us-central1-a" --machine_type="f1-micro" --network="default" --external_ip_address="ephemeral" --service_account_scopes="https://www.googleapis.com/auth/userinfo.email,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/devstorage.full_control" --image="https://www.googleapis.com/compute/v1/projects/dart-carte-du-jour/global/images/dart-engine-v1" --persistent_boot_disk="true" --metadata_from_file=startup-script:$STARTUP_SCRIPT
+gcutil --service_version="v1" --project="dart-carte-du-jour" addinstance "test-instance" --zone="us-central1-a" --machine_type="g1-small" --network="default" --external_ip_address="ephemeral" --service_account_scopes="https://www.googleapis.com/auth/userinfo.email,https://www.googleapis.com/auth/compute,https://www.googleapis.com/auth/devstorage.full_control" --image="https://www.googleapis.com/compute/v1/projects/dart-carte-du-jour/global/images/dart-engine-v1" --persistent_boot_disk="true" --auto_delete_boot_disk="true" --metadata_from_file=startup-script:$STARTUP_SCRIPT
--- scripts/quick_launch/startup-script.sh
+++ scripts/quick_launch/startup-script.sh
@@ -7,3 +7,9 @@ sed -i '1i Port 443' /etc/ssh/sshd_config
 
 # restart sshd
 /etc/init.d/ssh restart
+
+
+# gsutil cp -r gs://dart-carte-du-jour/configurations/github_private_repo_pull ~/
+# sudo -H -u financeCoding bash -c 'echo "I am $USER, with uid $UID"' 
+sudo -H -u financeCoding bash -c 'gsutil cp -r gs://dart-carte-du-jour/configurations/github_private_repo_pull ~/ && cd ~/github_private_repo_pull && bash ./clone_project.sh'
+sudo -H -u financeCoding bash -c 'source /etc/profile && cd ~/github_private_repo_pull/dart-carte-du-jour && pub install && dart bin/package_daemon.dart'
