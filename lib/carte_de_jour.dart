78178fc9ccc011026b21afd0a5cc60a85827053c
Adam Singer <financeCoding@gmail.com>
2014-04-02 16:30:34 -0700

    models and unittests

--- lib/carte_de_jour.dart
+++ lib/carte_de_jour.dart
@@ -1,19 +1,68 @@
 library carte_de_jour;
 
-class Version {
-
-}
-
 class Package {
   List<String> uploaders;
   String name;
-  List<Version> versions;
+  List<String> versions;
+  Package.fromJson(Map data) {
+    uploaders = new List<String>();
+    if (data.containsKey('uploaders')) {
+      for (var u in data['uploaders']) {
+        uploaders.add(u);
+      }
+    }
+
+    if (data.containsKey('name')) {
+      name = data['name'];
+    }
+
+    versions = new List<String>();
+    if (data.containsKey('versions')) {
+      versions.addAll(data['versions'].toList());
+    }
+  }
 }
 
 class PubPackages {
   String prev;
-  List<Package> packages;
+  List<String> packages;
   String pages;
   String next;
+  PubPackages.fromJson(Map data) {
+    if (data.containsKey('prev')) {
+      prev = data['prev'];
+    }
+
+    if (data.containsKey('pages')) {
+      pages = data['pages'];
+    }
+
+    if (data.containsKey('next')) {
+      next = data['next'];
+    }
+
+    packages = new List<Package>();
+    if (data.containsKey('packages')) {
+      for (var p in data['packages']) {
+        packages.add(p);
+      }
+    }
+  }
+}
+
+String generatePubSpecFile(String packageName, String packageVersion, String mockPackageName) {
+  StringBuffer pubSpecData = new StringBuffer()
+  ..writeln("name: $mockPackageName")
+  ..writeln("dependencies:")
+  ..writeln("  $packageName: '$packageVersion'");
+  return pubSpecData.toString();
+}
+
+build_docs() {
+  //
+}
+
+fetch_packages() {
+
 }
 
--- test/test_runner.dart
+++ test/test_runner.dart
@@ -1,4 +1,72 @@
+import 'dart:convert';
+
+import 'package:unittest/unittest.dart';
+import 'package:unittest/vm_config.dart';
+
+// TODO(adam): rename dart-carte-du-jour to dart_carte_du_jour
+import 'package:dart-carte-du-jour/carte_de_jour.dart';
 
 void main() {
-  print("hello world");
+
+  group('pub spec', () {
+    test('create pub spec', () {
+      String packageName = "unittest";
+      String packageVersion = "0.10.0";
+      String mockPackageName = "mock_unittest";
+
+
+      String pubSpecFile = generatePubSpecFile(packageName, packageVersion, mockPackageName);
+      String pubSpecFileExpected = """name: mock_unittest
+dependencies:
+  unittest: '0.10.0'
+""";
+      expect(pubSpecFile, pubSpecFileExpected);
+    });
+  });
+
+  group('models', () {
+    test('Package.fromJson', () {
+      String unittestPackageModel = """
+      {
+    "name": "unittest",
+    "uploaders": [
+        "dgrove@google.com",
+        "jmesserly@google.com"
+    ],
+    "versions": [
+        "0.10.0",
+        "0.10.1",
+        "0.10.1+1"
+    ]
+}""";
+      Package package = new Package.fromJson(JSON.decode(unittestPackageModel));
+      expect(package.name, equals("unittest"));
+      expect(package.versions.length, equals(3));
+      expect(package.versions.contains('0.10.0'), isTrue);
+      expect(package.uploaders.length, equals(2));
+      expect(package.uploaders.contains('dgrove@google.com'), isTrue);
+
+    });
+
+    test('PubPackages.fromJson', () {
+      String packages = """
+      {
+    "next": "http://pub.dartlang.org/packages.json?page=2",
+    "packages": [
+        "http://pub.dartlang.org/packages/tags.json",
+        "http://pub.dartlang.org/packages/smartcanvas.json",
+        "http://pub.dartlang.org/packages/annotation_crawler.json"
+    ],
+    "pages": 15,
+    "prev": null
+}
+      """;
+      PubPackages pubPackages = new PubPackages.fromJson(JSON.decode(packages));
+      expect(pubPackages.next, equals("http://pub.dartlang.org/packages.json?page=2"));
+      expect(pubPackages.pages, equals(15));
+      expect(pubPackages.prev, isNull);
+      expect(pubPackages.packages.length, equals(3));
+      expect(pubPackages.packages[0], equals("http://pub.dartlang.org/packages/tags.json"));
+    });
+  });
 }
