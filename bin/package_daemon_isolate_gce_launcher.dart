7d0b833ae472920d83e62f15f5f62b3be6f54797
Adam Singer <financeCoding@gmail.com>
2014-04-18 11:33:45 -0700

    add the building of packages

--- bin/package_daemon_isolate_gce_launcher.dart
+++ bin/package_daemon_isolate_gce_launcher.dart
@@ -6,13 +6,13 @@ import 'package:quiver/collection.dart';
 
 import 'package:dart_carte_du_jour/carte_de_jour.dart';
 
-final int MAX_GCE_INSTANCES = 10;
+final int MAX_GCE_INSTANCES = 3;
 
 class IsolateGceLauncher {
 
   int _gceInstances = 0;
 
-  Duration _timeout;
+  Duration _timeout = const Duration(seconds: 1);
   Timer _timer;
 
   Queue<Package> buildQueue = new Queue<Package>();
@@ -28,37 +28,45 @@ class IsolateGceLauncher {
   start() {
     isolateQueueServiceSendPort.send(isolateQueueServiceReceivePort.sendPort);
     _initListeners();
-    _timeout = new Duration(seconds: 1);
+//    _timeout = new Duration(seconds: 10);
     // TODO: consider using Timer.run();
-    _timer = new Timer.periodic(_timeout, callback);
+//    _timer = new Timer.periodic(_timeout, callback);
+    Timer.run(callback);
   }
 
-  void callback(Timer timer) {
+  void callback() {
 
     if (_gceInstances < MAX_GCE_INSTANCES && buildQueue.isNotEmpty) {
       // TODO: query GCE instead of keeping local counter.
-      buildingQueue.add(buildQueue.removeFirst());
+      Package package = buildQueue.removeFirst();
+      buildingQueue.add(package);
+
       _gceInstances++;
       print("starting new gce instance, _gceInstances = ${_gceInstances}");
+      // TODO: support builder version ranges
+      deployDocumentationBuilder(package, package.versions.first);
 
-      // TODO: Replace with deployDocumentationBuilder
-      new Future.delayed(new Duration(seconds:1), () {
-        print('a gce instance has completed, _gceInstances = ${_gceInstances}');
-        // documentationInstanceAlive();
-        completedQueue.add(buildingQueue.removeFirst());
-        _gceInstances--;
-
-
-        while (completedQueue.isNotEmpty) {
-          // TODO: only send what has been completed.
-          isolateQueueServiceSendPort.send({'command': 'packageBuildComplete',
-            'message': completedQueue.removeFirst().toJson() });
-        }
-      });
     } else {
       // TODO: check the current number of build instances on gce
       print("waiting till available gce instance or buildQueue is empty");
     }
+
+    // TODO: Might be better to check if the `package_build_info.json`
+    // file was uploaded.
+    completedQueue.addAll(buildingQueue
+        .where((p) => !documentationInstanceAlive(p, p.versions.first)).toList());
+
+
+    while (completedQueue.isNotEmpty) {
+      // TODO: only send what has been completed.
+      isolateQueueServiceSendPort.send({'command': 'packageBuildComplete',
+        'message': completedQueue.removeFirst().toJson() });
+      _gceInstances--;
+    }
+
+    // Timer.run(callback);
+    // Do not want periodic timer here.
+    new Timer(_timeout, callback);
   }
 
   _initListeners() {
