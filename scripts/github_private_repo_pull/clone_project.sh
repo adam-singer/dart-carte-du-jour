1a0c0d1c287a07e3f2000fade303dc4832214e32
Adam Singer <financeCoding@gmail.com>
2014-04-20 13:06:17 -0700

    removing temporary scripts and keys for dart-carte-du-jour repo

--- scripts/github_private_repo_pull/clone_project.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -x
-#GIT_SSH=git_ssh_wrapper git clone github-deploy-dart-carte-du-jour-pull:financeCoding/dart-carte-du-jour.git
-chmod go-rwx dart-carte-du-jour-pull
-chmod +x git_ssh_wrapper
-GIT_SSH=./git_ssh_wrapper git clone git@github.com:financeCoding/dart-carte-du-jour.git
