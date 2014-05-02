73d97f16fafc57885fd7464f93356f52d4220f1c
Adam Singer <financeCoding@gmail.com>
2014-05-01 18:24:08 -0700

    adding more tests for config class

--- test/src/test_google_compute_engine_config.dart
+++ test/src/test_google_compute_engine_config.dart
@@ -14,5 +14,34 @@ void main() {
       expect(googleComputeEngineConfig.serviceAccountEmail, equals("blah@blah.com"));
       expect(googleComputeEngineConfig.rsaPrivateKey, equals("xyz"));
     });
+
+    test('fromJson', () {
+      GoogleComputeEngineConfig googleComputeEngineConfig =
+          new GoogleComputeEngineConfig("123", "456", "blah@blah.com", "xyz");
+      var json = googleComputeEngineConfig.toJson();
+      GoogleComputeEngineConfig googleComputeEngineConfig2 =
+          new GoogleComputeEngineConfig.fromJson(json);
+      expect(googleComputeEngineConfig.projectId, equals("123"));
+      expect(googleComputeEngineConfig.projectNumber, equals("456"));
+      expect(googleComputeEngineConfig.serviceAccountEmail, equals("blah@blah.com"));
+      expect(googleComputeEngineConfig.rsaPrivateKey, equals("xyz"));
+    });
+
+    test('toJson', () {
+      GoogleComputeEngineConfig googleComputeEngineConfig =
+          new GoogleComputeEngineConfig("123", "456", "blah@blah.com", "xyz");
+      var json = googleComputeEngineConfig.toJson();
+      expect(json['projectId'], equals('123'));
+      expect(json['projectNumber'], equals('456'));
+      expect(json['serviceAccountEmail'], equals('blah@blah.com'));
+      expect(json['rsaPrivateKey'], equals('xyz'));
+    });
+
+    test('toString', () {
+      GoogleComputeEngineConfig googleComputeEngineConfig =
+          new GoogleComputeEngineConfig("123", "456", "blah@blah.com", "xyz");
+      var jsonString = googleComputeEngineConfig.toString();
+      expect('{"projectId":"123","projectNumber":"456","serviceAccountEmail":"blah@blah.com","rsaPrivateKey":"xyz"}', equals(jsonString));
+    });
   });
 }
